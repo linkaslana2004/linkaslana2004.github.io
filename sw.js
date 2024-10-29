a23b75c8ace56106bff747b9aa75eb837475a3df
jin 汪 <3237618960@qq.com>
2024-10-29 15:18:10 +0800

    Site updated: 2024-10-29 15:18:09

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1730183681769";
+const VERSION = "1730186286790";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
